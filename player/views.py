02f3820c7a5b46489cdfc2466766384fd5e365e4
Paul-Emmanuel Vandenburie <paulvand@cisco.com>
2018-01-23 15:24:11 +0100

    Use CreateView for SignUp page.

--- player/views.py
+++ player/views.py
@@ -1,3 +1,7 @@
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
@@ -45,3 +49,8 @@ def accept_invitation(request, id):
     else:
         return render(request, "player/accept_invitation_form.html",
                         {'invitation': invitation})
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = "player/signup_form.html"
+    success_url = reverse_lazy('player_home')
